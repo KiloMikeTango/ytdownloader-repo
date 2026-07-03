252a0ac310bb0c1dfbff927fdbe84e855128b0e6
Kaung Myat Thu 🇲🇲 <161342107+KiloMikeTango@users.noreply.github.com>
2026-07-03 16:17:06 +0630

    Refactor install.sh for clarity and conciseness
    Removed unnecessary lines and updated echo message for clarity.

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# 1. Add your repository to the Termux apt sources
 mkdir -p $PREFIX/etc/apt/sources.list.d
 echo "deb https://kilomiketango.github.io/ytdownloader-repo stable main" > $PREFIX/etc/apt/sources.list.d/ytdownloader.list
-
-# 2. Update and install
 pkg update
 pkg install ytdownloader -y
-
-echo "Install လုပ်ပြီးပါပြီ!'ytdownloader' လို့ရိုက်ပြီးသုံးနိုင်ပါပြီ။"
+echo "Install လုပ်ပြီးပါပြီ! [ytdownloader] လို့ရိုက်ပြီးသုံးနိုင်ပါပြီ။"
