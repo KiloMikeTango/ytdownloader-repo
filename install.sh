119d7f55f2c96ac1deccb8b9b3bcff8d1da25dbe
Kaung Myat Thu 🇲🇲 <161342107+KiloMikeTango@users.noreply.github.com>
2026-07-10 07:56:53 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p $PREFIX/etc/apt/sources.list.d
-# The trailing '/' is very important here
+# Use './' so apt automatically detects the root repository structure
 echo "deb [trusted=yes] https://kilomiketango.github.io/ytdownloader-repo/ ./" > $PREFIX/etc/apt/sources.list.d/ytdownloader.list
 pkg update
 pkg install ytdownloader -y
+echo "Install လုပ်ပြီးပါပြီ!"
