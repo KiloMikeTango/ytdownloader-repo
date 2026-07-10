1cf63c83a85da40f2c9fc4c538aac09cf71ae476
Kaung Myat Thu 🇲🇲 <161342107+KiloMikeTango@users.noreply.github.com>
2026-07-10 07:48:09 +0630

    Update ytdownloader repository URL in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p $PREFIX/etc/apt/sources.list.d
-echo "deb https://kilomiketango.github.io/ytdownloader-repo stable main" > $PREFIX/etc/apt/sources.list.d/ytdownloader.list
+# Notice the addition of 'dists' in the URL below
+echo "deb https://kilomiketango.github.io/ytdownloader-repo dists/stable main" > $PREFIX/etc/apt/sources.list.d/ytdownloader.list
 pkg update
 pkg install ytdownloader -y
 echo "Install လုပ်ပြီးပါပြီ! [ytdownloader] လို့ရိုက်ပြီးသုံးနိုင်ပါပြီ။"
