a3c918be297cc93472720bcfd29cd0a30e6fb78b
Kaung Myat Thu 🇲🇲 <161342107+KiloMikeTango@users.noreply.github.com>
2026-07-10 07:52:43 +0630

    Fix ytdownloader repository URL in install.sh
    Updated repository URL format and added trusted flag.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p $PREFIX/etc/apt/sources.list.d
-# Notice the addition of 'dists' in the URL below
-echo "deb https://kilomiketango.github.io/ytdownloader-repo dists/stable main" > $PREFIX/etc/apt/sources.list.d/ytdownloader.list
+# The trailing '/' is very important here
+echo "deb [trusted=yes] https://kilomiketango.github.io/ytdownloader-repo/ ./" > $PREFIX/etc/apt/sources.list.d/ytdownloader.list
 pkg update
 pkg install ytdownloader -y
-echo "Install လုပ်ပြီးပါပြီ! [ytdownloader] လို့ရိုက်ပြီးသုံးနိုင်ပါပြီ။"
